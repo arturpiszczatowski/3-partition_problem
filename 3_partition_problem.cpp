b3f4769aa37cdc51d9cc6a9e1cc5e3bb6a18f9e3
Artur Piszczatowski <s20487@pjwstk.edu.pl>
2021-10-11 12:01:54 +0200

    Raw

--- 3_partition_problem.cpp
+++ 3_partition_problem.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <functional>
+#include <fstream>
+#include<sstrea>
+#include<vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+using my_multiset = vector<double>;
+
+using my_triplets = vector<vector<double>>;
+
+bool test_for_triplets(my_multiset multiset){
+    if(!sizeof multiset % 3 == 0){
+        cout << "Multiset can't be divided into triplets.";
+        return false;
+    }
+    return true;
+}
+
+int test_for_target(my_multiset multiset){
+    test_for_triplets(multiset);
+    int number_of_triplets = multiset.size()/3;
+
+
+    auto sum_of_numbers = [=]() -> int {
+        int sum = 0;
+        for(int i : multiset){
+            sum += i;
+        }
+        return sum;
+    };
+
+    int target = sum_of_numbers / number_of_triplets;
+
+    return target;
+}
+
+
+int main(int argc, char** argv) {
 }
